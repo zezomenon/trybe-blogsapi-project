3864e443eb81f61efe155ea120cf44922520dfeb
Israel Menon <zezomenon@gmail.com>
2021-06-06 12:10:53 -0300

    Refatorando jwt secret dotenv novamente

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { User } = require('../models');
 
-const JWT_SECRET = '123456';
+const JWT_SECRET = process.env.JWT_SECRET || '123456';
 
 const generateToken = async (userEmail) => {
   const { dataValues } = await User.findOne({ where: { email: userEmail } });
